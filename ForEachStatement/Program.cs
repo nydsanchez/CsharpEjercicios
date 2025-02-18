6a6f61f0bd3879ad2f2e78b1d893751e4d27144c
Nydia Massiel Sánchez <66643813+nydsanchez@users.noreply.github.com>
2025-02-17 21:55:25 -0600

    Foreach basic exercise

---
+++ ForEachStatement/Program.cs
@@ -0,0 +1,23 @@
+﻿string[] names = [ "Rowena", "Robin", "Bao" ];
+foreach (string name in names)
+{
+    Console.WriteLine(name);
+}
+
+/*FOREACH IN JAVASCRIP
+const name = ["Rowena", "Robin", "Bao"];
+
+name.forEach((element) => console.log(element));
+
+*/
+
+int[] inventory = { 200, 450, 700, 175, 250 };
+int total=0;
+int bin = 0;
+foreach (int product in inventory)
+{
+    total+=product;
+    bin++;
+    Console.WriteLine($"Bin {bin} = {product} items (Running total: {total})");
+}
+Console.WriteLine($"We have {total} items in inventory.");
