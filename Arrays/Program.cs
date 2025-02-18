ca531cf39548574f68af3b3c2e93a65a398cfed9
Nydia Massiel Sánchez <66643813+nydsanchez@users.noreply.github.com>
2025-02-17 21:32:58 -0600

    array C# exercises

---
+++ Arrays/Program.cs
@@ -0,0 +1,24 @@
+﻿//string[] fraudulentOrderIDs = new string[3]; // array statement
+
+//setting data into an array
+/*
+
+fraudulentOrderIDs[0] = "A123";
+fraudulentOrderIDs[1] = "B456";
+fraudulentOrderIDs[2] = "C789";
+
+*/
+
+//other way to setting data
+string[] fraudulentOrderIDs = [ "A123", "B456", "C789" ];
+
+
+Console.WriteLine($"First: {fraudulentOrderIDs[0]}");
+Console.WriteLine($"Second: {fraudulentOrderIDs[1]}");
+Console.WriteLine($"Third: {fraudulentOrderIDs[2]}");
+
+//reassign the value of an array
+fraudulentOrderIDs[0] = "F000";
+Console.WriteLine($"Reassign First: {fraudulentOrderIDs[0]}");
+
+Console.WriteLine($"There are {fraudulentOrderIDs.Length} fraudulent orders to process.");
